0ac3f29c132b75587f43f089ae1dbff0c819b374
cenfv <59135587+cenfv@users.noreply.github.com>
2024-03-29 14:51:28 -0300

    Added .env configuration and PostgreSQL database connection setup

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"github.com/desafio-estagio/initializers"
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDB()
+}
 
 func main() {
-	fmt.Println("Olá Guardião!")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
 }
